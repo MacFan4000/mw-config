8839fcc13e13147bbbe55282cacc42898803e244
MacFan4000 <paulfkeffer@gmail.com>
2021-06-10 15:09:59 -0400

    1.36 sitenotice
    per discussed day and time in -sre

--- Sitenotice.php
+++ Sitenotice.php
@@ -9,7 +9,7 @@ if ( $wmgSiteNoticeOptOut ) {
 // Increment this version number whenever you change the site notice
 // and don't comment it out
 $wgMajorSiteNoticeID = 50;
-/*
+
 // Write your SiteNotice below.  Comment out this section to disable.
 if ( !$wmgSiteNoticeOptOut ) {
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
@@ -18,11 +18,11 @@ if ( !$wmgSiteNoticeOptOut ) {
 
 		$siteNotice .= <<<EOF
 				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td style="font-size:125%">Miraheze has upgraded to the latest version of MediaWiki (1.35)! However, if you experience any issues, please do let us know on <a href="https://phabricator.miraheze.org/">Phabricator</a>.</td>
+				<td style="font-size:125%">Miraheze plans to upgrade to the latest version of MediaWiki (1.36) on Saturday (12 June, 2021) from 19:00 UTC time to approximately 21:00 UTC. Please save your edits 5 minutes before hand.</td>
 				</tr></tbody></table>
 EOF;
 
 		return true;
 	}
 }
-*/
+
